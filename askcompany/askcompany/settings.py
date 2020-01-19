0d148720b076acda755aa84747d8cc3294f48a53
bora shin <d6ymmd@gmail.com>
2020-01-20 01:28:45 +0900

    EP25: static files

--- askcompany/askcompany/settings.py
+++ askcompany/askcompany/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7i$zo0_4_sk(ud+sr3is*55miof$^o2ds=7s@q(-69b#jl78s0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -129,5 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askcompany', 'static'),
+]
 
 INTERNAL_IPS = ['127.0.0.1']
