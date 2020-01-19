70851ba64c9b9af3d6f72e420ea67ec9e1e09c30
bora shin <d6ymmd@gmail.com>
2020-01-19 13:24:10 +0900

    django template engine

--- askcompany/askcompany/settings.py
+++ askcompany/askcompany/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7i$zo0_4_sk(ud+sr3is*55miof$^o2ds=7s@q(-69b#jl78s0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -61,8 +61,11 @@ ROOT_URLCONF = 'askcompany.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'askcompany', 'templates'),
+        ],
         'APP_DIRS': True,
+        #False일 경우 app하위의 templates는 사용하지 않는다
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
